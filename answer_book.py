f196120bea22c1c3a02d905a39027045a0c4c910
MicroSic <62823814+MicroSic@users.noreply.github.com>
2025-04-18 10:32:37 +0900

    Update answer_book.py

--- answer_book.py
+++ answer_book.py
@@ -1,19 +1,12 @@
-import sys
 import requests
-import io
 import linecache
-sys.stdout = io.TextIOWrapper(sys.stdout.detach(), encoding = 'utf-8')
 
-return_string = ''
+max = 350
 path = 'Answer_Book.txt'
 #修改max的值为Answer_Book.txt的行数
-max="350"
-response = requests.get("https://www.random.org/integers/?num=1&min=1&max="+max+"&col=1&base=10&format=plain&rnd=new")
-x=response.text
 
-f = open(path,"r",encoding='UTF-8')   #设置文件对象
-i=int(x)
-content=linecache.getline(path, i)
-f.close() #关闭文件
-
-print(content)
+response = requests.get(f"https://www.random.org/integers/?num=1&min=1&max={max}&col=1&base=10&format=plain&rnd=new")
+i = int(response.text.strip())
+#设置文件对象
+line = linecache.getline(path, i).strip()
+print(line)
